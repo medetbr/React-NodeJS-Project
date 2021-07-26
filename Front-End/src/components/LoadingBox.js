63e066b95dde132e1623cb62e61301cd45a42d28
Medet Berimen <berimen35@gmail.com>
2021-07-26 18:47:45 +0300

    Backend created

---
+++ Front-End/src/components/LoadingBox.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+const LoadingBox = () => {
+    return (
+        <div>
+            <i className="fa fa-spinner fa-spin"/> Loading...
+        </div>
+    )
+}
+export default LoadingBox
--- Front-End/src/screens/HomeScreen.js
+++ Front-End/src/screens/HomeScreen.js
@@ -1,17 +1,43 @@
-import React from "react";
-import data from "../data";
+import React, { useEffect, useState } from "react";
 import Product from "../components/Product";
+import axios from "axios";
+import LoadingBox from "../components/LoadingBox";
+import MessageBox from "../components/MessageBox";
 
 const HomeScreen = () => {
-    return (
-        <div className="row center">
-        {data.products.map((product) => (                      
-   <Product key={product._id} product = {product}/>
-  
-  ))}
+  const [products, setProducts] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(false);
 
- 
-</div>
-    )
-}
-export default HomeScreen
+  useEffect(() => {
+    const fecthData = async () => {
+      try {
+        setLoading(true);
+        const { data } = await axios.get("http://localhost:5000/api/products");
+        setLoading(false);
+        setProducts(data);
+      } catch (err) {
+        setError(err.message);
+        setLoading(false);
+      }
+    };
+    fecthData();
+  }, []);
+
+  return (
+    <div>
+      {loading ? (
+        <LoadingBox />
+      ) : error ? (
+        <MessageBox variant="danger">{error}</MessageBox>
+      ) : (
+        <div className="row center">
+          {products.map((product) => (
+            <Product key={product._id} product={product} />
+          ))}
+        </div>
+      )}
+    </div>
+  );
+};
+export default HomeScreen;
