6514df88a991da601932c503f6c67f6cf796d400
Medet Berimen <berimen35@gmail.com>
2021-07-27 10:24:26 +0300

    Redux installed and store component created

---
+++ Front-End/src/redux/store.js
@@ -0,0 +1,15 @@
+import { applyMiddleware, compose, createStore } from "redux"
+import data from "../data"
+import thunk from "redux-thunk"
+
+const initialState = {}
+
+const reducer = (state,action) => {
+    return {
+        products: data.products
+    }
+}
+const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
+const store = createStore(reducer, initialState, composeEnhancer(applyMiddleware(thunk)))
+
+export default store
