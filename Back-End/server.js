d5a82513ba64fdd1fd0a6022452c5112c295a71f
Medet Berimen <berimen35@gmail.com>
2021-07-31 15:52:25 +0300

    User checking in backend

--- Back-End/server.js
+++ Back-End/server.js
@@ -1,9 +1,11 @@
 import express from "express";
-import data from "./data.js"
+import dotenv from "dotenv"
 import cors from "cors"
 import mongoose  from "mongoose";
 import userRouter from "./routers/userRouter.js"
 import productRouter from "./routers/productRouter.js";
+
+dotenv.config();
 
 const app = express();
 
@@ -15,25 +17,15 @@ mongoose.connect("mongodb://localhost/e-commerce", {
 })
 
 app.use(cors())
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.use("/api/users", userRouter);
 app.use("/api/products", productRouter);
 /*
-app.get("/api/products/:id", (req, res) => {
-   
-    const product = data.products.find((x) => x._id === req.params.id)
-    
-    if (product) {
-        res.send(product)
-        
-    }
-    else {
-         res.status(404).send({message: "Product Not Found"})
-    }
-})*/
 app.use((err, res, req, next) => {
     res.status(500).send({ message: err.message });
-})
+})*/
 
 
 const port = process.env.PORT || 5000;
